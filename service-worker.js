03630d45e404632cb5dc90f058581c0bdd373490
takada291 <tkp291@gmail.com>
2026-01-23 22:58:15 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'white-board-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css'
+];
+
+// インストール時にキャッシュする
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// リクエスト時にキャッシュから返す
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // キャッシュにあればそれを返す
+        if (response) {
+          return response;
+        }
+        // なければネットに取りに行く
+        return fetch(event.request);
+      })
+  );
+});
