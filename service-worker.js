2d2b18e88832749d6ac892667896f58d3faa1b4a
takada291 <tkp291@gmail.com>
2026-01-26 05:55:38 +0900

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'white-board-v1.3.1';
+const CACHE_NAME = 'white-board-v1.3.2';
 const urlsToCache = [
   './',
   './index.html',
@@ -31,6 +31,7 @@ self.addEventListener('fetch', function(event) {
   );
 
 });
+
 
 
 
